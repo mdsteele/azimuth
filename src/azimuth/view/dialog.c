998bc60b652d538b1ed6d83fe2fb4b570d1d0ed3
Matthew D. Steele <mdsteele@alum.mit.edu>
2013-04-18 10:47:01 -0400

    Implement drawing CPU A dialog portrait

--- src/azimuth/view/dialog.c
+++ src/azimuth/view/dialog.c
@@ -38,9 +38,40 @@ static GLuint portrait_display_lists_start;
 // Offsets from portrait_display_lists_start for various portraits:
 #define HOPPER_PAUSED_INDEX 0
 #define HOPPER_TALKING_INDEX 1
-#define NUM_PORTRAIT_DISPLAY_LISTS 2
+#define CPU_A_PAUSED_INDEX 2
+#define NUM_PORTRAIT_DISPLAY_LISTS 3
 
 /*===========================================================================*/
+
+static void draw_computer_circle(
+    double cx, double cy, az_color_t color1, az_color_t color2) {
+  const double r1 = 3;
+  const double r2 = 5;
+  glBegin(GL_QUAD_STRIP); {
+    for (int i = 0; i <= 360; i += 30) {
+      const double c = cos(AZ_DEG2RAD(i));
+      const double s = sin(AZ_DEG2RAD(i));
+      glColor4ub(color1.r, color1.g, color1.b, color1.a);
+      glVertex2d(cx + r1 * c, cy + r1 * s);
+      glColor4ub(color2.r, color2.g, color2.b, color2.a);
+      glVertex2d(cx + r2 * c, cy + r2 * s);
+    }
+  } glEnd();
+}
+
+static void draw_computer_blinkenlight(
+    double cx, double cy, az_color_t color1) {
+  const double r = 2;
+  glBegin(GL_TRIANGLE_FAN); {
+    glColor4ub(color1.r, color1.g, color1.b, color1.a);
+    glVertex2d(cx, cy);
+    for (int i = 0; i <= 360; i += 30) {
+      const double c = cos(AZ_DEG2RAD(i));
+      const double s = sin(AZ_DEG2RAD(i));
+      glVertex2d(cx + r * c, cy + r * s);
+    }
+  } glEnd();
+}
 
 static void draw_portrait_polygon(
     double bezel, az_color_t color1, az_color_t color2, az_polygon_t polygon) {
@@ -129,6 +160,28 @@ static const az_vector_t hopper_talking_vertices[] = {
   {100, 0}, {100, 100}, {0, 100}, {0, 0}
 };
 
+static const az_vector_t cpu_a1_vertices[] = {
+  {14, 100}, {0, 100}, {0, 0}, {37, 0}, {37, 37}, {36, 37}, {36, 40}, {30, 40},
+  {30, 46}, {27, 46}, {27, 47},
+  // Bend in pipe:
+  {17, 47}, {15, 48}, {14, 50}
+};
+static const az_vector_t cpu_a2_vertices[] = {
+  {57, 0}, {57, 37}, {56, 37}, {56, 40}, {44, 40}, {44, 37}, {43, 37}, {43, 0}
+};
+static const az_vector_t cpu_a3_vertices[] = {
+  {100, 0}, {100, 47}, {73, 47}, {73, 46}, {70, 46}, {70, 40},
+  {64, 40}, {64, 37}, {63, 37}, {63, 0}
+};
+static const az_vector_t cpu_a4_vertices[] = {
+  {100, 100}, {63, 100}, {63, 63}, {64, 63}, {64, 60},
+  {70, 60}, {70, 54}, {73, 54}, {73, 53}, {100, 53}
+};
+static const az_vector_t cpu_a5_vertices[] = {
+  {20, 100}, {20, 53}, {27, 53}, {27, 54}, {30, 54},
+  {30, 60}, {56, 60}, {56, 63}, {57, 63}, {57, 100}
+};
+
 /*===========================================================================*/
 
 void az_init_portrait_drawing(void) {
@@ -139,13 +192,29 @@ void az_init_portrait_drawing(void) {
   // Compile Hopper portrait:
   glNewList(portrait_display_lists_start + HOPPER_PAUSED_INDEX, GL_COMPILE); {
     const az_polygon_t polygon = AZ_INIT_POLYGON(hopper_paused_vertices);
-    draw_portrait_polygon(4.5, (az_color_t){0, 255, 255, 255},
-                          (az_color_t){0, 255, 255, 0}, polygon);
+    draw_portrait_polygon(5, (az_color_t){0, 255, 255, 255},
+                          (az_color_t){255, 32, 0, 0}, polygon);
   } glEndList();
   glNewList(portrait_display_lists_start + HOPPER_TALKING_INDEX, GL_COMPILE); {
     const az_polygon_t polygon = AZ_INIT_POLYGON(hopper_talking_vertices);
-    draw_portrait_polygon(4.5, (az_color_t){0, 255, 255, 255},
-                          (az_color_t){0, 255, 255, 0}, polygon);
+    draw_portrait_polygon(5, (az_color_t){0, 255, 255, 255},
+                          (az_color_t){255, 32, 0, 0}, polygon);
+  } glEndList();
+  // Compile CPU A portrait:
+  glNewList(portrait_display_lists_start + CPU_A_PAUSED_INDEX, GL_COMPILE); {
+    const az_color_t color1 = {255, 128, 255, 255};
+    const az_color_t color2 = {255, 0, 255, 0};
+    const az_polygon_t polygon1 = AZ_INIT_POLYGON(cpu_a1_vertices);
+    draw_portrait_polygon(6, color1, color2, polygon1);
+    const az_polygon_t polygon2 = AZ_INIT_POLYGON(cpu_a2_vertices);
+    draw_portrait_polygon(6, color1, color2, polygon2);
+    const az_polygon_t polygon3 = AZ_INIT_POLYGON(cpu_a3_vertices);
+    draw_portrait_polygon(6, color1, color2, polygon3);
+    const az_polygon_t polygon4 = AZ_INIT_POLYGON(cpu_a4_vertices);
+    draw_portrait_polygon(6, color1, color2, polygon4);
+    const az_polygon_t polygon5 = AZ_INIT_POLYGON(cpu_a5_vertices);
+    draw_portrait_polygon(6, color1, color2, polygon5);
+    draw_computer_circle(40, 50, color1, (az_color_t){255, 0, 255, 128});
   } glEndList();
   // TODO: Compile other portraits.
 }
@@ -162,7 +231,12 @@ void az_draw_portrait(az_portrait_t portrait, bool talking, az_clock_t clock) {
                        HOPPER_TALKING_INDEX : HOPPER_PAUSED_INDEX);
       break;
     case AZ_POR_HQ: return; // TODO
-    case AZ_POR_CPU_A: return; // TODO
+    case AZ_POR_CPU_A:
+      display_list += CPU_A_PAUSED_INDEX;
+      if (az_clock_mod(2, (talking ? 3 : 25), clock)) {
+        draw_computer_blinkenlight(40, 50, (az_color_t){255, 128, 255, 255});
+      }
+      break;
     case AZ_POR_CPU_B: return; // TODO
     case AZ_POR_CPU_C: return; // TODO
     case AZ_POR_TRICHORD: return; // TODO
