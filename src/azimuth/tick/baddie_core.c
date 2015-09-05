a08d7037a7f80fe3891084f367064c4da17d827f
Matthew D. Steele <mdsteele@alum.mit.edu>
2015-09-05 18:18:04 -0400

    More progress on Zenith Core

--- src/azimuth/tick/baddie_core.c
+++ src/azimuth/tick/baddie_core.c
@@ -34,21 +34,44 @@
 
 // Primary states:
 #define INITIAL_STATE 0
-#define START_DORMANCY_STATE 1
-#define DORMANT_STATE 2
-#define RAINBOW_BEAM_STATE 3
-#define PILLBOX_STATE 4
-#define PRISMATIC_STATE 5
-#define STARBURST_STATE 6
-#define RAINBOW_BEAM_WITH_MINES_STATE 7
-#define BUZZSAW_STATE 8
-#define REVERSE_BUZZSAW_STATE 9
+#define DORMANT_STATE 1
+#define RAINBOW_BEAM_1_STATE 2
+#define RAINBOW_BEAM_2_STATE 3
+#define PILLBOX_1_STATE 4
+#define PILLBOX_2_STATE 5
+#define PRISMATIC_1_STATE 6
+#define PRISMATIC_2_STATE 7
+#define BUZZSAW_1_STATE 8
+#define BUZZSAW_2_STATE 9
+#define STARBURST_1_STATE 10
+#define STARBURST_2_STATE 11
+#define STARBURST_3_STATE 12
 // Rainbow beam secondary states:
 #define BEAM_CHARGE_FORWARD 0
 #define BEAM_CHARGE_BACKWARD 1
 #define BEAM_FIRE_FORWARD 2
 #define BEAM_FIRE_BACKWARD 3
 #define BEAM_COOLDOWN 4
+
+/*===========================================================================*/
+
+static const struct {
+  int next;
+  double frac;
+} transition_table[] = {
+  [DORMANT_STATE]        = { .next = RAINBOW_BEAM_1_STATE, .frac = 1 },
+  [RAINBOW_BEAM_1_STATE] = { .next = PILLBOX_1_STATE,      .frac = 0.9 },
+  [PILLBOX_1_STATE]      = { .next = BUZZSAW_1_STATE,      .frac = 0.775 },
+  [BUZZSAW_1_STATE]      = { .next = STARBURST_1_STATE,    .frac = 0.65 },
+  [STARBURST_1_STATE]    = { .next = PRISMATIC_1_STATE,    .frac = 1 },
+  [PRISMATIC_1_STATE]    = { .next = RAINBOW_BEAM_2_STATE, .frac = 0.5 },
+  [RAINBOW_BEAM_2_STATE] = { .next = BUZZSAW_2_STATE,      .frac = 0.4 },
+  [BUZZSAW_2_STATE]      = { .next = STARBURST_2_STATE,    .frac = 0.275 },
+  [STARBURST_2_STATE]    = { .next = PILLBOX_2_STATE,      .frac = 1 },
+  [PILLBOX_2_STATE]      = { .next = STARBURST_3_STATE,    .frac = 0.15 },
+  [STARBURST_3_STATE]    = { .next = PRISMATIC_2_STATE,    .frac = 1 },
+  [PRISMATIC_2_STATE]    = { .next = PRISMATIC_2_STATE,    .frac = 0 }
+};
 
 /*===========================================================================*/
 
@@ -60,6 +83,10 @@ static int get_secondary_state(az_baddie_t *baddie) {
   return (baddie->state >> 8) & 0xff;
 }
 
+static int get_tertiary_state(az_baddie_t *baddie) {
+  return (baddie->state >> 16) & 0xff;
+}
+
 static void set_primary_state(az_baddie_t *baddie, int primary) {
   assert(primary >= 0 && primary <= 0xff);
   baddie->state = primary;
@@ -68,6 +95,11 @@ static void set_primary_state(az_baddie_t *baddie, int primary) {
 static void set_secondary_state(az_baddie_t *baddie, int secondary) {
   assert(secondary >= 0 && secondary <= 0xff);
   baddie->state = (baddie->state & 0xff) | (secondary << 8);
+}
+
+static void set_tertiary_state(az_baddie_t *baddie, int tertiary) {
+  assert(tertiary >= 0 && tertiary <= 0xff);
+  baddie->state = (baddie->state & 0xffff) | (tertiary << 16);
 }
 
 /*===========================================================================*/
@@ -178,8 +210,8 @@ static void pull_gravity_in_and_out(
 static void init_rainbow_beam(az_space_state_t *state, az_baddie_t *baddie,
                               bool lay_mines) {
   assert(baddie->kind == AZ_BAD_ZENITH_CORE);
-  set_primary_state(baddie, (lay_mines ? RAINBOW_BEAM_WITH_MINES_STATE :
-                             RAINBOW_BEAM_STATE));
+  set_primary_state(baddie, (lay_mines ? RAINBOW_BEAM_2_STATE :
+                             RAINBOW_BEAM_1_STATE));
   set_secondary_state(baddie,
                       (az_vdot(az_vsub(state->ship.position, baddie->position),
                                az_vpolar(1.0, baddie->angle)) >= 0.0 ?
@@ -216,30 +248,78 @@ static void init_rainbow_beam(az_space_state_t *state, az_baddie_t *baddie,
 
 static void init_buzzsaw(az_baddie_t *baddie, bool reverse) {
   assert(baddie->kind == AZ_BAD_ZENITH_CORE);
-  set_primary_state(baddie, (reverse ? REVERSE_BUZZSAW_STATE : BUZZSAW_STATE));
+  set_primary_state(baddie, (reverse ? BUZZSAW_2_STATE : BUZZSAW_1_STATE));
   baddie->param = 0.0;
   baddie->cooldown = 2.0;
 }
 
-static void init_pillbox(az_baddie_t *baddie) {
+static void init_pillbox(az_baddie_t *baddie, bool super) {
   assert(baddie->kind == AZ_BAD_ZENITH_CORE);
-  set_primary_state(baddie, PILLBOX_STATE);
+  set_primary_state(baddie, (super ? PILLBOX_2_STATE : PILLBOX_1_STATE));
   baddie->param = 1.0;
   baddie->cooldown = 1.0;
 }
 
-static void init_prismatic(az_baddie_t *baddie) {
+static void init_prismatic(az_baddie_t *baddie, bool super) {
   assert(baddie->kind == AZ_BAD_ZENITH_CORE);
-  set_primary_state(baddie, PRISMATIC_STATE);
+  set_primary_state(baddie, (super ? PRISMATIC_2_STATE : PRISMATIC_1_STATE));
   baddie->param = 0.0;
   baddie->cooldown = 2.0;
 }
 
-static void init_starburst(az_baddie_t *baddie) {
+static void init_starburst(az_baddie_t *baddie, int num_blasts) {
   assert(baddie->kind == AZ_BAD_ZENITH_CORE);
-  set_primary_state(baddie, STARBURST_STATE);
+  assert(num_blasts >= 1 && num_blasts <= 3);
+  set_primary_state(baddie, (num_blasts == 1 ? STARBURST_1_STATE :
+                             num_blasts == 2 ? STARBURST_2_STATE :
+                             STARBURST_3_STATE));
+  set_secondary_state(baddie, num_blasts);
   baddie->param = 0.0;
   baddie->cooldown = 0.0;
+}
+
+static bool try_transition(az_space_state_t *state, az_baddie_t *baddie) {
+  assert(baddie->kind == AZ_BAD_ZENITH_CORE);
+  const int primary = get_primary_state(baddie);
+  if (baddie->health <=
+      transition_table[primary].frac * baddie->data->max_health) {
+    switch (transition_table[primary].next) {
+      case RAINBOW_BEAM_1_STATE:
+        init_rainbow_beam(state, baddie, false);
+        return true;
+      case RAINBOW_BEAM_2_STATE:
+        init_rainbow_beam(state, baddie, true);
+        return true;
+      case PILLBOX_1_STATE:
+        init_pillbox(baddie, false);
+        return true;
+      case PILLBOX_2_STATE:
+        init_pillbox(baddie, true);
+        return true;
+      case BUZZSAW_1_STATE:
+        init_buzzsaw(baddie, false);
+        return true;
+      case BUZZSAW_2_STATE:
+        init_buzzsaw(baddie, true);
+        return true;
+      case PRISMATIC_1_STATE:
+        init_prismatic(baddie, false);
+        return true;
+      case PRISMATIC_2_STATE:
+        init_prismatic(baddie, true);
+        return true;
+      case STARBURST_1_STATE:
+        init_starburst(baddie, 1);
+        return true;
+      case STARBURST_2_STATE:
+        init_starburst(baddie, 2);
+        return true;
+      case STARBURST_3_STATE:
+        init_starburst(baddie, 3);
+        return true;
+    }
+  }
+  return false;
 }
 
 /*===========================================================================*/
@@ -358,23 +438,22 @@ static void do_rainbow_beam(az_space_state_t *state, az_baddie_t *baddie,
       break;
     case BEAM_COOLDOWN:
       if (baddie->cooldown <= 0.0) {
-        if (!lay_mines && baddie->health <= 0.9 * baddie->data->max_health) {
-          init_pillbox(baddie);
-        } else if (lay_mines &&
-                   baddie->health <= 0.5 * baddie->data->max_health) {
-          init_buzzsaw(baddie, true);
-        } else init_rainbow_beam(state, baddie, lay_mines);
+        if (!try_transition(state, baddie)) {
+          init_rainbow_beam(state, baddie, lay_mines);
+        }
       }
       break;
   }
 }
 
-static void fire_pillbox_rockets(az_space_state_t *state, az_baddie_t *baddie,
-                                 double time) {
+static void do_pillbox_1(az_space_state_t *state, az_baddie_t *baddie,
+                         double time) {
   assert(baddie->kind == AZ_BAD_ZENITH_CORE);
   adjust_to_pillbox_configuration(baddie, time);
   pull_gravity_in_and_out(state, baddie, time);
-  baddie->angle = az_mod2pi(baddie->angle + AZ_DEG2RAD(30) * time);
+  const int secondary = get_secondary_state(baddie);
+  const double turn_rate = (secondary == 1 ? -1 : 1) * AZ_DEG2RAD(30);
+  baddie->angle = az_mod2pi(baddie->angle + turn_rate * time);
   if (baddie->cooldown <= 0.0) {
     const double angle = AZ_DEG2RAD(45) *
       round((az_vtheta(az_vsub(state->ship.position, baddie->position)) -
@@ -382,9 +461,45 @@ static void fire_pillbox_rockets(az_space_state_t *state, az_baddie_t *baddie,
     az_fire_baddie_projectile(state, baddie, AZ_PROJ_ROCKET,
                               100.0, angle, 0.0);
     az_play_sound(&state->soundboard, AZ_SND_FIRE_ROCKET);
-    if (baddie->health <= 0.8 * baddie->data->max_health) {
-      init_buzzsaw(baddie, false);
-    } else baddie->cooldown = 0.5;
+    if (!try_transition(state, baddie)) {
+      const int remaining = get_tertiary_state(baddie) - 1;
+      if (remaining > 0) {
+        set_tertiary_state(baddie, remaining);
+      } else {
+        set_secondary_state(baddie, (secondary == 0 ? 1 : 0));
+        set_tertiary_state(baddie, az_randint(3, 9));
+      }
+      baddie->cooldown = 0.5;
+    }
+  }
+}
+
+static void do_pillbox_2(az_space_state_t *state, az_baddie_t *baddie,
+                         double time) {
+  assert(baddie->kind == AZ_BAD_ZENITH_CORE);
+  adjust_to_pillbox_configuration(baddie, time);
+  pull_gravity_in_and_out(state, baddie, time);
+  const int secondary = get_secondary_state(baddie);
+  if (secondary == 0) {
+    if (baddie->cooldown <= 0.0 && !try_transition(state, baddie)) {
+      set_secondary_state(baddie, az_randint(1, 2));
+      baddie->cooldown = 0.3;
+    }
+  } else {
+    const double turn_rate = (secondary == 1 ? -1 : 1) * AZ_DEG2RAD(60);
+    baddie->angle = az_mod2pi(baddie->angle + turn_rate * time);
+    if (baddie->cooldown <= 0.0) {
+      const double angle = AZ_DEG2RAD(45) *
+        round((az_vtheta(az_vsub(state->ship.position, baddie->position)) -
+               baddie->angle) / AZ_DEG2RAD(45));
+      for (int i = -1; i <= 1; ++i) {
+        az_fire_baddie_projectile(state, baddie, AZ_PROJ_ROCKET,
+                                  100.0, angle, i * AZ_DEG2RAD(10));
+      }
+      az_play_sound(&state->soundboard, AZ_SND_FIRE_ROCKET);
+      set_secondary_state(baddie, 0);
+      baddie->cooldown = 0.3;
+    }
   }
 }
 
@@ -405,9 +520,9 @@ static void fire_prismatic_walls(az_space_state_t *state, az_baddie_t *baddie,
                                 112.0, start_angle + AZ_DEG2RAD(i), 0.0);
     }
     az_play_sound(&state->soundboard, AZ_SND_FIRE_GUN_PIERCE);
-    if (baddie->health <= 0.6 * baddie->data->max_health) {
-      init_rainbow_beam(state, baddie, true);
-    } else baddie->cooldown = 1.0;
+    if (!try_transition(state, baddie)) {
+      baddie->cooldown = 1.0;
+    }
   }
 }
 
@@ -418,13 +533,13 @@ static void do_starburst(az_space_state_t *state, az_baddie_t *baddie,
   adjust_gravity(state, time, -400.0, false);
   const double max_turn_rate = AZ_DEG2RAD(720);
   const double turn_accel = AZ_DEG2RAD(200);
-  switch (get_secondary_state(baddie)) {
+  switch (get_tertiary_state(baddie)) {
     case 0:
       // Accelerate spinning:
       baddie->param = fmin(baddie->param + turn_accel * time, max_turn_rate);
       baddie->angle = az_mod2pi(baddie->angle + baddie->param * time);
       if (baddie->param >= max_turn_rate) {
-        set_secondary_state(baddie, 1);
+        set_tertiary_state(baddie, 1);
         baddie->cooldown = az_random(0.5, 2.0);
       }
       break;
@@ -441,7 +556,7 @@ static void do_starburst(az_space_state_t *state, az_baddie_t *baddie,
         }
         if (fabs(angle_delta) <= AZ_DEG2RAD(8)) {
           baddie->angle = az_mod2pi(baddie->angle + angle_delta);
-          set_secondary_state(baddie, 2);
+          set_tertiary_state(baddie, 2);
           baddie->cooldown = 0.1;
         }
       }
@@ -455,14 +570,20 @@ static void do_starburst(az_space_state_t *state, az_baddie_t *baddie,
                                     0.0);
         }
         az_play_sound(&state->soundboard, AZ_SND_FIRE_HYPER_ROCKET);
-        set_secondary_state(baddie, 3);
+        set_tertiary_state(baddie, 3);
         baddie->cooldown = 0.5;
       }
       break;
     case 3:
       // Proceed to next state (after a brief pause):
       if (baddie->cooldown <= 0.0) {
-        init_prismatic(baddie);
+        const int blasts_remaining = get_secondary_state(baddie) - 1;
+        if (blasts_remaining > 0) {
+          set_secondary_state(baddie, blasts_remaining);
+          set_tertiary_state(baddie, 1);
+        } else if (!try_transition(state, baddie)) {
+          init_starburst(baddie, 1);
+        }
       }
       break;
   }
@@ -484,9 +605,9 @@ static void do_buzzsaw(az_space_state_t *state, az_baddie_t *baddie,
                               120.0, AZ_DEG2RAD(45) * secondary, angle);
     az_play_sound(&state->soundboard, AZ_SND_FIRE_FIREBALL);
     set_secondary_state(baddie, az_modulo(secondary - 3, 8));
-    if (!reverse && baddie->health <= 0.7 * baddie->data->max_health) {
-      init_starburst(baddie);
-    } else baddie->cooldown = az_random(1.0, 1.5) * (reverse ? 0.4 : 0.5);
+    if (!try_transition(state, baddie)) {
+      baddie->cooldown = az_random(1.0, 1.5) * (reverse ? 0.4 : 0.5);
+    }
   }
 }
 
@@ -509,37 +630,41 @@ void az_tick_bad_zenith_core(az_space_state_t *state, az_baddie_t *baddie,
     case INITIAL_STATE:
       baddie->temp_properties |= AZ_BADF_INVINCIBLE;
       break;
-    case START_DORMANCY_STATE:
-      baddie->temp_properties |= AZ_BADF_INVINCIBLE;
-      baddie->cooldown = 10.0;
-      set_primary_state(baddie, DORMANT_STATE);
-      break;
     case DORMANT_STATE:
       baddie->temp_properties |= AZ_BADF_INVINCIBLE;
-      if (baddie->cooldown <= 0.0) {
-        init_rainbow_beam(state, baddie, false);
+      if (get_secondary_state(baddie) == 0) {
+        set_secondary_state(baddie, 1);
+        baddie->cooldown = 10.0;
+      } else if (baddie->cooldown <= 0.0) {
+        try_transition(state, baddie);
       }
       break;
-    case RAINBOW_BEAM_STATE:
+    case RAINBOW_BEAM_1_STATE:
       do_rainbow_beam(state, baddie, time, false);
       break;
-    case PILLBOX_STATE:
-      fire_pillbox_rockets(state, baddie, time);
-      break;
-    case PRISMATIC_STATE:
-      fire_prismatic_walls(state, baddie, time);
-      break;
-    case STARBURST_STATE:
-      do_starburst(state, baddie, time);
-      break;
-    case RAINBOW_BEAM_WITH_MINES_STATE:
+    case RAINBOW_BEAM_2_STATE:
       do_rainbow_beam(state, baddie, time, true);
       break;
-    case BUZZSAW_STATE:
+    case PILLBOX_1_STATE:
+      do_pillbox_1(state, baddie, time);
+      break;
+    case PILLBOX_2_STATE:
+      do_pillbox_2(state, baddie, time);
+      break;
+    case BUZZSAW_1_STATE:
       do_buzzsaw(state, baddie, time, false);
       break;
-    case REVERSE_BUZZSAW_STATE:
+    case BUZZSAW_2_STATE:
       do_buzzsaw(state, baddie, time, true);
+      break;
+    case PRISMATIC_1_STATE:
+    case PRISMATIC_2_STATE:
+      fire_prismatic_walls(state, baddie, time);
+      break;
+    case STARBURST_1_STATE:
+    case STARBURST_2_STATE:
+    case STARBURST_3_STATE:
+      do_starburst(state, baddie, time);
       break;
     default:
       set_primary_state(baddie, DORMANT_STATE);
