5116a7e67a6ceac20071cddf26f2aedf99bc41d7
Matthew D. Steele <mdsteele@alum.mit.edu>
2015-10-01 08:43:42 -0400

    Bump version to 0.1.2

--- src/azimuth/version.h
+++ src/azimuth/version.h
@@ -25,7 +25,7 @@
 
 #define AZ_VERSION_MAJOR 0
 #define AZ_VERSION_MINOR 1
-#define AZ_VERSION_PATCH 1
+#define AZ_VERSION_PATCH 2
 
 /*===========================================================================*/
 
